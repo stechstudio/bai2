5a0aabe75e32a6c0a270c5cc8890398cafae6638
Zeb DeOs <zebulin@stechstudio.com>
2021-09-30 17:18:20 -0400

    Import the LineParser prototype from bai2-demo...
    ...which still needs automated tests and to be put into service by the
    records.

---
+++ src/LineParser.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace STS\Bai2;
+
+class LineParser
+{
+
+    private int $numFieldsYielded = 0;
+
+    private string $buffer;
+
+    public function __construct(private string $line, private int $numFields = 0)
+    {
+        $this->buffer = $line;
+    }
+
+    public function hasNext(): bool
+    {
+        return !$this->isEndOfLine();
+    }
+
+    public function next(): ?string
+    {
+        if ($this->isEndOfLine()) {
+            return null;
+        } else {
+            return $this->shift();
+        }
+    }
+
+    public function setNumFields($numFields): self
+    {
+        $this->numFields = $numFields;
+        return $this;
+    }
+
+    public function rewind(): self
+    {
+        $this->buffer = $this->line;
+        $this->numFieldsYielded = 0;
+
+        return $this;
+    }
+
+    protected function shift(): string
+    {
+        $this->numFieldsYielded++;
+
+        if ($this->isLastExpectedField()) {
+            return $this->buffer;
+        }
+
+        [$field, $rest] = $this->bisect();
+        $this->buffer = $rest;
+
+        return $field;
+    }
+
+    protected function bisect(): array
+    {
+        $field = '';
+        $rest = '/';
+
+        $exploded = explode(',', $this->buffer, 2);
+        if (count($exploded) == 1) {
+            $field = rtrim($exploded[0], '/');
+        } else {
+            $field = $exploded[0];
+            $rest = $exploded[1];
+        }
+
+        return [$field, $rest];
+    }
+
+    protected function isEndOfLine(): bool
+    {
+        if ($this->numFields) {
+            return $this->numFieldsYielded >= $this->numFields;
+        } else {
+            return $this->buffer == '/';
+        }
+    }
+
+    protected function isLastExpectedField(): bool
+    {
+        if ($this->numFields) {
+            return $this->numFieldsYielded == $this->numFields;
+        }
+
+        return false;
+    }
+
+}
