9d5d1412badf52b5503881d70bcda0a6a609de67
Zeb DeOs <zebulin@stechstudio.com>
2021-10-04 09:18:01 -0400

    Move the iterator out of the way...
    ...for it seems like the wrong abstraction. Will probably get rid of it
    before long, but holding on to it until we get the more fundamental
    field-at-a-time parser worked out.

--- src/Parsers/LineParser.php
+++ src/Parsers/LineParser.php
@@ -2,54 +2,6 @@
 
 namespace STS\Bai2\Parsers;
 
-class LineParser implements \Iterator
+class LineParser
 {
-
-    protected LineParserBuffer $buffer;
-
-    protected ?string $current;
-
-    protected int $index;
-
-    public function __construct(
-        protected string $line,
-        protected int $numFields = 0
-    ) {
-        $this->rewind();
-    }
-
-    public function current(): string
-    {
-        return $this->current;
-    }
-
-    public function key(): int
-    {
-        return $this->index;
-    }
-
-    public function next(): void
-    {
-        $this->index++;
-        $this->current = $this->buffer->next();
-    }
-
-    public function rewind(): void
-    {
-        $this->index = 0;
-        $this->buffer = new LineParserBuffer($this->line, $this->numFields);
-        $this->current = $this->buffer->next();
-    }
-
-    public function reset(int $numFields): void
-    {
-        $this->numFields = $numFields;
-        $this->rewind();
-    }
-
-    public function valid(): bool
-    {
-        return $this->buffer->valid();
-    }
-
 }
