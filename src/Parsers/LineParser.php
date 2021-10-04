c96cd45247c30f667fd64e0fbb2f2058722319dc
Zeb DeOs <zebulin@stechstudio.com>
2021-10-04 10:40:48 -0400

    Implement first-stab of LineParser::peek()

--- src/Parsers/LineParser.php
+++ src/Parsers/LineParser.php
@@ -4,4 +4,32 @@ namespace STS\Bai2\Parsers;
 
 class LineParser
 {
+
+    protected LineParserBuffer $buffer;
+
+    protected array $cache = [];
+
+    protected int $cursor = -1;
+
+    public function __construct(string $line)
+    {
+        $this->buffer = new LineParserBuffer($line);
+    }
+
+    public function peek(): ?string
+    {
+        return $this->fetch($this->index + 1);
+    }
+
+    protected function fetch(int $index): ?string
+    {
+        // TODO(zmd): perfect place for null-coalescing assignment op
+        //    $this->cache[$index] ??= ...
+        if (!array_key_exists($index, $this->cache)) {
+            $this->cache[$index] = $this->buffer->next();
+        }
+
+        return $this->cache[$index];
+    }
+
 }
