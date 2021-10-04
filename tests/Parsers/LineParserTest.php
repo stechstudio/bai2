b11e0fc8404e13f6acc9010b59fac9ded1b76cd3
Zeb DeOs <zebulin@stechstudio.com>
2021-10-04 15:46:59 -0400

    LineParser: test happy paths for ::takeText()

--- tests/Parsers/LineParserTest.php
+++ tests/Parsers/LineParserTest.php
@@ -9,6 +9,10 @@ final class LineParserTest extends TestCase
 
     private static string $headerLine = '01,SENDR1,RECVR1,210616,1700,01,80,10,2/';
 
+    private static string $transactionLine = "16,003,10000,D,3,1,1000,5,10000,30,25000,123456789,987654321,The following character is, of all the path separation characters I've ever used, my absolute favorite: /";
+
+    private static string $transactionLineDefaultedText = "16,003,10000,D,3,1,1000,5,10000,30,25000,123456789,987654321,/";
+
     public function testPeekReturnsNextFieldWithoutConsumingIt()
     {
         $parser = new LineParser(self::$headerLine);
@@ -42,6 +46,34 @@ final class LineParserTest extends TestCase
         $parser = new LineParser(self::$headerLine);
         $this->assertEquals(['01', 'SENDR1', 'RECVR1'], $parser->takeN(3));
         $this->assertEquals('210616', $parser->peek());
+    }
+
+    public function testTakeTextReturnsTheRemainderOfBufferAsText()
+    {
+        $parser = new LineParser(self::$transactionLine);
+
+        // consume and discard the non-text fields
+        $parser->take(13);
+
+        // Note: per the spec, text fields are always at the end of a line, and
+        // you have to at least partially parse a line first to know at what
+        // position that text field begins.
+        $this->assertEquals(
+            "The following character is, of all the path separation characters I've ever used, my absolute favorite: /",
+            $parser->takeText()
+        );
+    }
+
+    public function testTakeTextReturnsEmptyStringForDefaultedTextField()
+    {
+        $parser = new LineParser(self::$transactionLineDefaultedText);
+
+        // consume and discard the non-text fields
+        $parser->take(13);
+
+        // Note: per the spec, text fields which are defaulted are denominated
+        // with ,/
+        $this->assertEquals('', $parser->takeText());
     }
 
 }
