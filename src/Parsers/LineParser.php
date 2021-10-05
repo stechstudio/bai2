98443b69cc81b8e1624e827cc593804cac4c1841
Zeb DeOs <zebulin@stechstudio.com>
2021-10-05 16:49:49 -0400

    LineParser: assert wishes on LineParserBuffer...
    ...breaking things temporarily. We will soon fix the LineParserBuffer to
    bring it in line with our current wishes.

--- src/Parsers/LineParser.php
+++ src/Parsers/LineParser.php
@@ -7,53 +7,40 @@ class LineParser
 
     protected LineParserBuffer $buffer;
 
-    // TODO(zmd): this is so terrible; refactor LineParserBuffer such that this
-    //   becomes unnecessary
-    protected string $line;
-
-    protected array $cache = [];
-
-    protected int $cursor = -1;
-
     public function __construct(string $line)
     {
         $this->buffer = new LineParserBuffer($line);
-        // TODO(zmd): this is so terrible; refactor LineParserBuffer such that
-        //   this becomes unnecessary
-        $this->line = $line;
     }
 
     public function peek(): ?string
     {
-        return $this->fetch($this->cursor + 1);
+        $value = $this->buffer->next->val();
+        $this->buffer->prev();
+
+        return $value;
     }
 
+    // TODO(zmd): we probably want to throw if user tries to drop more than
+    //   what is available in the buffer?
     public function drop(int $numToDrop): array
     {
         $slice = [];
         for (; $numToDrop; --$numToDrop) {
-            // TODO(zmd): we probably want to throw if user tries to drop more
-            //   than what is available to drop.
             $slice[] = $this->shift();
         }
         return $slice;
     }
 
+    // TODO(zmd): we probably want to throw if user tries to shift beyond end
+    //   of buffer?
     public function shift(): ?string
     {
-        $this->cursor++;
-        return $this->fetch($this->cursor);
+        return $this->buffer->next()->val();
     }
 
     public function shiftText(): ?string
     {
-        // TODO(zmd): this is so terrible; refactor LineParserBuffer such that
-        //   this becomes unnecessary
-        $this->buffer = new LineParserBuffer($this->line, $this->cursor + 2);
-        for ($counter = $this->cursor + 1; $counter; --$counter) {
-            $this->buffer->next();
-        }
-        return $this->shift();
+        return $this->buffer->next()->text();
     }
 
     // TODO(zmd): ponder whether we want to keep this, and if we do write
@@ -61,23 +48,6 @@ class LineParser
     public function isEndOfLine(): bool
     {
         return $this->buffer->isEndOfLine();
-    }
-
-    protected function fetch(int $index): ?string
-    {
-        // TODO(zmd): perfect place for null-coalescing assignment op
-        //    $this->cache[$index] ??= ...
-        if (!array_key_exists($index, $this->cache)) {
-            $next = $this->buffer->next();
-
-            if (is_null($next)) {
-                return null;
-            } else {
-                $this->cache[$index] = $next;
-            }
-        }
-
-        return $this->cache[$index];
     }
 
 }
