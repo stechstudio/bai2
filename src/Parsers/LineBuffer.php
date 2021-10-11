23ebb406faef97b95291ccfc4e92b34fe034f984
Zeb DeOs <zebulin@stechstudio.com>
2021-10-11 17:27:15 -0400

    LineBuffer: *rips out a tuft of hair*

--- src/Parsers/LineBuffer.php
+++ src/Parsers/LineBuffer.php
@@ -15,14 +15,18 @@ class LineBuffer
         protected string $line,
         public ?int $physicalRecordLength = null
     ) {
-        $this->endOfLine = strlen($line);
+        if (!is_null($this->physicalRecordLength)) {
+            $this->line = rtrim($this->line);
+        }
+
+        $this->endOfLine = strlen($this->line);
     }
 
     public function eat(): self
     {
         if ($this->textTaken) {
             $this->cursor = $this->endOfLine;
-        } else if ($this->isEndOfLine()) {
+        } else if ($this->isEndOfLine() || $this->isEndOfLogicalRecord()) {
             throw new \Exception('Cannot advance beyond the end of the buffer.');
         } else {
             $this->cursor = $this->findFieldEnd() + 1;
@@ -33,7 +37,7 @@ class LineBuffer
 
     public function field(): string
     {
-        if ($this->isEndOfLine()) {
+        if ($this->isEndOfLine() || $this->isEndOfLogicalRecord()) {
             throw new \Exception('Cannot access fields at the end of the buffer.');
         }
 
@@ -59,6 +63,13 @@ class LineBuffer
     public function isEndOfLine(): bool
     {
         return $this->cursor == $this->endOfLine;
+    }
+
+    public function isEndOfLogicalRecord(): bool
+    {
+        // echo 'substr($this->line, $this->cursor - 1, 1): ';
+        // var_export(substr($this->line, $this->cursor - 1, 1));
+        return $this->cursor > 0 && substr($this->line, $this->cursor - 1, 1) === '/';
     }
 
     protected function seek(string $needle): ?int {
