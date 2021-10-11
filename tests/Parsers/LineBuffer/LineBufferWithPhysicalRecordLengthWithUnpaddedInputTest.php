b70bdd2db34fe04bef798b2a4449909d8008ad1d
Zeb DeOs <zebulin@stechstudio.com>
2021-10-11 11:16:27 -0400

    LineBuffer: experiment/prep - re-use test logic for input-based variations

--- tests/Parsers/LineBuffer/LineBufferWithPhysicalRecordLengthWithUnpaddedInputTest.php
+++ tests/Parsers/LineBuffer/LineBufferWithPhysicalRecordLengthWithUnpaddedInputTest.php
@@ -6,79 +6,97 @@ use PHPUnit\Framework\TestCase;
 
 final class LineBufferWithPhysicalRecordLengthWithUnpaddedInputTest extends TestCase
 {
-    private LineBuffer $buffer;
 
-    protected function setUp(): void
-    {
-        $this->buffer = new LineBuffer('foo,bar,baz/', 80);
+    protected LineBuffer $buffer;
+
+    protected array $lines = [
+        'terminated' => 'foo,bar,baz/',
+        'unterminated' => 'foo,bar,baz quux',
+        'defaulted-field' => 'foo,,baz/',
+        'defaulted-text-field' => 'foo,bar,/',
+    ];
+
+    protected function buffer(): LineBuffer {
+        return $this->setBuffer()->buffer;
+    }
+
+    protected function setBuffer(
+        string $desiredBuffer = 'terminated',
+        int $physicalRecordLength = 80
+    ): self {
+        $this->buffer ??= new LineBuffer(
+            $this->lines[$desiredBuffer],
+            $physicalRecordLength
+        );
+        return $this;
     }
 
     public function testRecognizesIsEndOfLineAfterEatingFields(): void
     {
-        $this->assertFalse($this->buffer->isEndOfLine());
-        $this->buffer->eat()->eat()->eat();
-        $this->assertTrue($this->buffer->isEndOfLine());
+        $this->assertFalse($this->buffer()->isEndOfLine());
+        $this->buffer()->eat()->eat()->eat();
+        $this->assertTrue($this->buffer()->isEndOfLine());
     }
 
     public function testCanAccessFields(): void
     {
-        $this->assertEquals('foo', $this->buffer->field());
-        $this->assertEquals('bar', $this->buffer->eat()->field());
-        $this->assertEquals('baz', $this->buffer->eat()->field());
+        $this->assertEquals('foo', $this->buffer()->field());
+        $this->assertEquals('bar', $this->buffer()->eat()->field());
+        $this->assertEquals('baz', $this->buffer()->eat()->field());
     }
 
     public function testCanAccessDefaultedField(): void
     {
-        $this->buffer = new LineBuffer('foo,,baz/', 80);
-        $this->buffer->eat();
-        $this->assertEquals('', $this->buffer->field());
+        $this->setBuffer('defaulted-field');
+        $this->buffer()->eat();
+        $this->assertEquals('', $this->buffer()->field());
     }
 
     public function testCanAccessTextField(): void
     {
-        $this->buffer = new LineBuffer('foo,bar,baz quux', 80);
-        $this->buffer->eat()->eat();
-        $this->assertEquals('baz quux', $this->buffer->textField());
+        $this->setBuffer('unterminated');
+        $this->buffer()->eat()->eat();
+        $this->assertEquals('baz quux', $this->buffer()->textField());
     }
 
     public function testCanAccessTextFieldWithCommasAndSlashes(): void
     {
-        $this->buffer->eat();
-        $this->assertEquals('bar,baz/', $this->buffer->textField());
+        $this->buffer()->eat();
+        $this->assertEquals('bar,baz/', $this->buffer()->textField());
     }
 
     public function testCanAccessDefaultedTextField(): void
     {
-        $this->buffer = new LineBuffer('foo,bar,/', 80);
+        $this->setBuffer('defaulted-text-field');
         $this->buffer->eat()->eat();
         $this->assertEquals('', $this->buffer->textField());
     }
 
     public function testThrowsWhenEatingPastEndOfInput(): void
     {
-        $this->buffer->eat()->eat()->eat();
+        $this->buffer()->eat()->eat()->eat();
 
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage('Cannot advance beyond the end of the buffer.');
-        $this->buffer->eat();
+        $this->buffer()->eat();
     }
 
     public function testThrowsWhenAccessingFieldPastEndOfInput(): void
     {
-        $this->buffer->eat()->eat()->eat();
+        $this->buffer()->eat()->eat()->eat();
 
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage('Cannot access fields at the end of the buffer.');
-        $this->buffer->field();
+        $this->buffer()->field();
     }
 
     public function testThrowsWhenAccessingTextFieldPastEndOfInput(): void
     {
-        $this->buffer->eat()->eat()->eat();
+        $this->buffer()->eat()->eat()->eat();
 
         $this->expectException(\Exception::class);
         $this->expectExceptionMessage('Cannot access fields at the end of the buffer.');
-        $this->buffer->textField();
+        $this->buffer()->textField();
     }
 
 }
