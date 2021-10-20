fe49d525ea017f7b692c1c7a07bf557f34f1ee3c
Zeb DeOs <zebulin@stechstudio.com>
2021-10-20 17:03:57 -0400

    AbstractEnvelopeRecord: replace bare exception...
    ...with a more appropriate (and soon to be re-used elsewhere) exception
    class.

---
+++ src/Exceptions/ExtantAssertionException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace STS\Bai2\Exceptions;
+
+/**
+ * Indicates that a Bai2 component was used in a clearly incorrect manner.
+ */
+class ExtantAssertionException extends \LogicException
+{
+
+}
