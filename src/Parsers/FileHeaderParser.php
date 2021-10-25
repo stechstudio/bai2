2a72a399be7b352ab33ac852347ffcf9430756f3
Zeb DeOs <zebulin@stechstudio.com>
2021-10-25 15:50:11 -0400

    FileHeaderParser: (WIP) simplify parse-once logic

--- src/Parsers/FileHeaderParser.php
+++ src/Parsers/FileHeaderParser.php
@@ -9,7 +9,7 @@ class FileHeaderParser
 
     protected MultilineParser $parser;
 
-    private array $rawFields = [];
+    protected array $parsed = [];
 
     public function push(string $line): self
     {
@@ -24,9 +24,8 @@ class FileHeaderParser
 
     public function offsetGet(string $key): string|int|null
     {
-        if (array_key_exists($key, $this->parse())) {
-            //$this->parse()[$key]
-            return $this->parseField($key, $this->parse()[$key]);
+        if (array_key_exists($key, $this->parseAll()->parsed)) {
+            return $this->parsed[$key];
         } else {
             throw new InvalidFieldNameException(
                 "File Header does not have a \"{$key}\" field."
@@ -34,78 +33,63 @@ class FileHeaderParser
         }
     }
 
-    protected function validate(string $value, string $longName): FieldParser
+    protected function parse(string $value, string $longName): FieldParser
     {
         return new FieldParser($value, $longName);
     }
 
     // ------------------------------------------------------------------------
 
-    private function parse(): array
+    private function parseAll(): self
     {
-        if (empty($this->rawFields)) {
-            $this->rawFields['recordCode'] = $this->parser->shift();
-            $this->rawFields['senderIdentification'] = $this->parser->shift();
-            $this->rawFields['receiverIdentification'] = $this->parser->shift();
-            $this->rawFields['fileCreationDate'] = $this->parser->shift();
-            $this->rawFields['fileCreationTime'] = $this->parser->shift();
-            $this->rawFields['fileIdentificationNumber'] = $this->parser->shift();
-            $this->rawFields['physicalRecordLength'] = $this->parser->shift();
-            $this->rawFields['blockSize'] = $this->parser->shift();
-            $this->rawFields['versionNumber'] = $this->parser->shift();
+        if (empty($this->parsed)) {
+            $this->parsed['recordCode'] =
+                $this->parse($this->parser->shift(), 'Record Code')
+                     ->is('01', 'must be "01"')
+                     ->string();
+
+            $this->parsed['senderIdentification'] =
+                $this->parse($this->parser->shift(), 'Sender Identification')
+                     ->match('/^[[:alnum:]]+$/', 'must be alpha-numeric')
+                     ->string();
+
+            $this->parsed['receiverIdentification'] =
+                $this->parse($this->parser->shift(), 'Receiver Identification')
+                     ->match('/^[[:alnum:]]+$/', 'must be alpha-numeric')
+                     ->string();
+
+            $this->parsed['fileCreationDate'] =
+                $this->parse($this->parser->shift(), 'File Creation Date')
+                     ->match('/^\d{6}$/', 'must be composed of exactly 6 numerals')
+                     ->string();
+
+            $this->parsed['fileCreationTime'] =
+                $this->parse($this->parser->shift(), 'File Creation Time')
+                     ->match('/^\d{4}$/', 'must be composed of exactly 4 numerals')
+                     ->string();
+
+            $this->parsed['fileIdentificationNumber'] =
+                $this->parse($this->parser->shift(), 'File Identification Number')
+                     ->match('/^\d+$/', 'must be composed of 1 or more numerals')
+                     ->int();
+
+            $this->parsed['physicalRecordLength'] =
+                $this->parse($this->parser->shift(), 'Physical Record Length')
+                     ->match('/^\d+$/', 'must be composed of 1 or more numerals')
+                     ->int(default: null);
+
+            $this->parsed['blockSize'] =
+                $this->parse($this->parser->shift(), 'Block Size')
+                     ->match('/^\d+$/', 'must be composed of 1 or more numerals')
+                     ->int(default: null);
+
+            $this->parsed['versionNumber'] =
+                $this->parse($this->parser->shift(), 'Version Number')
+                     ->is('2', 'must be "2" (this library only supports v2 of the BAI format)')
+                     ->string();
         }
 
-        return $this->rawFields;
-    }
-
-    private function parseField(string $key, string $value): string|int|null
-    {
-        return match ($key) {
-            'recordCode' =>
-                $this->validate($value, 'Record Code')
-                     ->is('01', 'must be "01"')
-                     ->string(),
-
-            'senderIdentification' =>
-                $this->validate($value, 'Sender Identification')
-                     ->match('/^[[:alnum:]]+$/', 'must be alpha-numeric')
-                     ->string(),
-
-            'receiverIdentification' =>
-                $this->validate($value, 'Receiver Identification')
-                     ->match('/^[[:alnum:]]+$/', 'must be alpha-numeric')
-                     ->string(),
-
-            'fileCreationDate' =>
-                $this->validate($value, 'File Creation Date')
-                     ->match('/^\d{6}$/', 'must be composed of exactly 6 numerals')
-                     ->string(),
-
-            'fileCreationTime' =>
-                $this->validate($value, 'File Creation Time')
-                     ->match('/^\d{4}$/', 'must be composed of exactly 4 numerals')
-                     ->string(),
-
-            'fileIdentificationNumber' =>
-                $this->validate($value, 'File Identification Number')
-                     ->match('/^\d+$/', 'must be composed of 1 or more numerals')
-                     ->int(),
-
-            'physicalRecordLength' =>
-                $this->validate($value, 'Physical Record Length')
-                     ->match('/^\d+$/', 'must be composed of 1 or more numerals')
-                     ->int(default: null),
-
-            'blockSize' =>
-                $this->validate($value, 'Block Size')
-                     ->match('/^\d+$/', 'must be composed of 1 or more numerals')
-                     ->int(default: null),
-
-            'versionNumber' =>
-                $this->validate($value, 'Version Number')
-                     ->is('2', 'must be "2" (this library only supports v2 of the BAI format)')
-                     ->string(),
-        };
+        return $this;
     }
 
 }
