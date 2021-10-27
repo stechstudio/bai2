514b52eac30d17b1326b544f82606499712dfecb
Zeb DeOs <zebulin@stechstudio.com>
2021-10-27 17:08:09 -0400

    GroupHeaderParser: stub out test & class

---
+++ tests/Parsers/GroupHeaderParserTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace STS\Bai2\Parsers;
+
+use STS\Bai2\Tests\Parsers\RecordParserTestCase;
+
+/**
+ * @group RecordParserTests
+ */
+final class GroupHeaderParserTest extends RecordParserTestCase
+{
+
+    protected static string $parserClass = GroupHeaderParser::class;
+
+    protected static string $readableParserName = 'Group Header';
+
+    protected static string $recordCode = '02';
+
+    protected static string $fullRecordLine = '02,ABC123,XYZ789,1,211027,0800,USD,2/';
+
+    protected static string $partialRecordLine = '02,ABC123,XYZ789/';
+
+    private static string $continuedRecordLine = '88,1,211027,0800,USD,2/';
+
+    // ----- record-specific parsing and usage ---------------------------------
+
+    // TODO(zmd): public function testParseFromSingleLine(): void {}
+
+    // TODO(zmd): public function testParseFromMultipleLines(): void {}
+
+    // TODO(zmd): public function testPhysicalRecordLengthEnforcedOnFirstLine(): void {}
+
+    // TODO(zmd): public function testPhysicalRecordLengthEnforcedOnSubsequentLine(): void {}
+
+    // TODO(zmd): public function testToArray(): void {}
+
+    // ----- record-specific field validation ----------------------------------
+
+    // TODO(zmd): test "Ultimate Receiver Identification" validation
+
+    // TODO(zmd): test "Originator Identification" validation
+
+    // TODO(zmd): test "Group Status" validation
+
+    // TODO(zmd): test "As-of-Date" validation
+
+    // TODO(zmd): test "As-of-Time" validation
+
+    // TODO(zmd): test "Currency Code" validation
+
+    // TODO(zmd): test "As-of-Date Modifier" validation
+
+}
