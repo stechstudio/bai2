b3ab56c8fc4681f6932c5006e35848393e047fb9
Zeb DeOs <zebulin@stechstudio.com>
2021-11-01 12:36:14 -0400

    AccountHeaderParser: extract ::shiftAndParseFundsType() to trait

--- src/Parsers/AccountHeaderParser.php
+++ src/Parsers/AccountHeaderParser.php
@@ -85,7 +85,7 @@ namespace STS\Bai2\Parsers;
  */
 final class AccountHeaderParser extends AbstractRecordParser
 {
-    use RecordParserTrait;
+    use RecordParserTrait, FundsTypeFieldParserTrait;
 
     public function __construct(protected ?int $physicalRecordLength = null)
     {
@@ -177,88 +177,6 @@ final class AccountHeaderParser extends AbstractRecordParser
         }
 
         return $this;
-    }
-
-    // TODO(zmd) extract parses funds type trait (used here and in transaction
-    //  parser)
-    protected function shiftAndParseFundsType(): array
-    {
-        $fundsType = [];
-
-        // TODO(zmd): validate format & default/optional
-        $fundsType['distributionOfAvailability'] =
-            $this->shiftAndParseField('Funds Type, Distribution of Availability')
-                 ->string(default: null);
-
-        // TODO(zmd): finish implementing me!
-        switch ($fundsType['distributionOfAvailability']) {
-            case 'Z':
-            case '0':
-            case '1':
-            case '2':
-                break;
-
-            case 'V':
-                // TODO(zmd): validate format & default/optional
-                $fundsType['valueDate'] =
-                    $this->shiftAndParseField('Funds Type, Value Dated, Date')
-                         ->string(default: null);
-
-                // TODO(zmd): validate format & default/optional
-                $fundsType['valueTime'] =
-                    $this->shiftAndParseField('Funds Type, Value Dated, Time')
-                         ->string(default: null);
-                break;
-
-            case 'S':
-                $fundsType['availability'] = [];
-
-                // TODO(zmd): validate format & default/optional
-                $fundsType['availability'][0] =
-                    $this->shiftAndParseField('Funds Type, Distributed Availability (S), Immediate Availability')
-                         ->int(default: null);
-
-                // TODO(zmd): validate format & default/optional
-                $fundsType['availability'][1] =
-                    $this->shiftAndParseField('Funds Type, Distributed Availability (S), One-day Availability')
-                         ->int(default: null);
-
-                // TODO(zmd): validate format & default/optional
-                $fundsType['availability'][2] =
-                    $this->shiftAndParseField('Funds Type, Distributed Availability (S), Two-or-more Day Availability')
-                         ->int(default: null);
-
-                break;
-
-            case 'D':
-                // TODO(zmd): parse D-type distributed availability funds type info
-                //   D,5,0,150000,1,100000,3,90000,5,70000,,7,50000
-                //     |
-                //     V
-                //   0 => 150000,
-                //   1 => 100000,
-                //   3 =>  90000,
-                //   5 =>  70000,
-                //   7 =>  50000,
-                // TODO(zmd): validate format & default/optional
-                $numDistributions =
-                    $this->shiftAndParseField('Funds Type, Distributed Availability (D), Number of Distributions')
-                         ->int(default: null);
-                for (; $numDistributions > 0; --$numDistributions) {
-                    // TODO(zmd): validate format & default/optional
-                    $days = $this->shiftAndParseField('Funds Type, Distributed Availability (D), Specified Availability')
-                                 ->int(default: null);
-
-                    // TODO(zmd): validate format & default/optional
-                    $amount = $this->shiftAndParseField('Funds Type, Distributed Availability (D), Amount')
-                                   ->int(default: null);
-
-                    $fundsType['availability'][$days] = $amount;
-                }
-                break;
-        }
-
-        return $fundsType;
     }
 
 }
---
+++ src/Parsers/FundsTypeFieldParserTrait.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace STS\Bai2\Parsers;
+
+trait FundsTypeFieldParserTrait
+{
+
+    protected function shiftAndParseFundsType(): array
+    {
+        $fundsType = [];
+
+        // TODO(zmd): validate format & default/optional
+        $fundsType['distributionOfAvailability'] =
+            $this->shiftAndParseField('Funds Type, Distribution of Availability')
+                 ->string(default: null);
+
+        // TODO(zmd): finish implementing me!
+        switch ($fundsType['distributionOfAvailability']) {
+            case 'Z':
+            case '0':
+            case '1':
+            case '2':
+                break;
+
+            case 'V':
+                // TODO(zmd): validate format & default/optional
+                $fundsType['valueDate'] =
+                    $this->shiftAndParseField('Funds Type, Value Dated, Date')
+                         ->string(default: null);
+
+                // TODO(zmd): validate format & default/optional
+                $fundsType['valueTime'] =
+                    $this->shiftAndParseField('Funds Type, Value Dated, Time')
+                         ->string(default: null);
+                break;
+
+            case 'S':
+                $fundsType['availability'] = [];
+
+                // TODO(zmd): validate format & default/optional
+                $fundsType['availability'][0] =
+                    $this->shiftAndParseField('Funds Type, Distributed Availability (S), Immediate Availability')
+                         ->int(default: null);
+
+                // TODO(zmd): validate format & default/optional
+                $fundsType['availability'][1] =
+                    $this->shiftAndParseField('Funds Type, Distributed Availability (S), One-day Availability')
+                         ->int(default: null);
+
+                // TODO(zmd): validate format & default/optional
+                $fundsType['availability'][2] =
+                    $this->shiftAndParseField('Funds Type, Distributed Availability (S), Two-or-more Day Availability')
+                         ->int(default: null);
+
+                break;
+
+            case 'D':
+                // TODO(zmd): validate format & default/optional
+                $numDistributions =
+                    $this->shiftAndParseField('Funds Type, Distributed Availability (D), Number of Distributions')
+                         ->int(default: null);
+                for (; $numDistributions > 0; --$numDistributions) {
+                    // TODO(zmd): validate format & default/optional
+                    $days = $this->shiftAndParseField('Funds Type, Distributed Availability (D), Specified Availability')
+                                 ->int(default: null);
+
+                    // TODO(zmd): validate format & default/optional
+                    $amount = $this->shiftAndParseField('Funds Type, Distributed Availability (D), Amount')
+                                   ->int(default: null);
+
+                    $fundsType['availability'][$days] = $amount;
+                }
+                break;
+        }
+
+        return $fundsType;
+    }
+
+}
